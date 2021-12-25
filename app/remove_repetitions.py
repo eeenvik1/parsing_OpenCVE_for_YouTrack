94a4e876602af3c3742c73ad637b24eae42c6d94
Bad_karma <49790977+eeenvik1@users.noreply.github.com>
2021-12-25 11:51:25 +0300

    Update remove_repetitions.py

--- app/remove_repetitions.py
+++ app/remove_repetitions.py
@@ -1,4 +1,5 @@
 import requests
+import re
 from dotenv import dotenv_values
 config = dotenv_values(".env")
 
@@ -10,23 +11,31 @@ headers = {
         "Authorization": "Bearer {}".format(YOU_TRACK_TOKEN),
         "Content-Type": "application/json"
     }
-list_summary = requests.get(main_url, headers=headers).json()
+list_summary = requests.get(URL, headers=headers).json()
+
+buff_cve_list = []
+buff_id_list = []
+for i in range(len(list_summary)):
+    regex = re.search(r'CVE-\d{4}-\d{4,6}', str(list_summary[i]['summary']))
+    if regex != None:
+        buff_cve_list.append(str(regex.group()))
+        buff_id_list.append(list_summary[i]['id'])
 
 no_repeat_list = []
 repeat_list = []
-for i in range(len(list_summary)):
-    if list_summary[i]['summary'] not in no_repeat_list:
-        no_repeat_list.append(list_summary[i]['summary'])
+for i in range(len(buff_cve_list)):
+    if buff_cve_list[i] not in no_repeat_list:
+        no_repeat_list.append(buff_cve_list[i])
     else:
-        repeat_list.append(list_summary[i]['id'])
+        repeat_list.append(buff_id_list[i])
 
 for n, name in enumerate(repeat_list, start = 1):
-    del_url = config.get("del_url")
-    delete_url = f'{del_url}/api/issues/{name}'
+    URL1 = f'https://vm-proval.myjetbrains.com/youtrack/api/issues/{name}'
     headers = {
         "Accept": "application/json",
         "Authorization": "Bearer {}".format(YOU_TRACK_TOKEN),
         "Content-Type": "application/json"
     }
-    requests.delete(delete_url, headers=headers)
+    requests.delete(URL1, headers=headers)
     print(f'{n} / {len(repeat_list)}')
+
