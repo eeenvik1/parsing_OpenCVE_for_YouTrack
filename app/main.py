7eb170ef7f126c74f5a20664f1f5373ca230aceb
Bad_karma <49790977+eeenvik1@users.noreply.github.com>
2022-02-17 10:17:07 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -11,6 +11,7 @@ import smtplib
 from email.message import EmailMessage
 config = dotenv_values(".env")
 
+
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 YOU_TRACK_TOKEN = config.get("YOU_TRACK_TOKEN")
 
@@ -44,7 +45,7 @@ def parsing_opencve():
     )
     # Get new CVE
     cve_line = []
-    for page_num in range(1, 10):
+    for page_num in range(1, 20):
         pagination = f'https://www.opencve.io/?page={page_num}'
         resp = s.get(pagination)
         parse = BeautifulSoup(resp.text, 'lxml')
@@ -53,11 +54,8 @@ def parsing_opencve():
             if index == -1:
                 cve_line.append(cve.text.replace(' is a new CVE', ''))
 
-    no_cve_line = []
-    for clear in cve_line:
-        no_cve_line.append(clear[:-1])
     cve_line_no_replic = []
-    for item in no_cve_line:
+    for item in cve_line:
         if item not in cve_line_no_replic:
             cve_line_no_replic.append(item)
     return cve_line_no_replic
@@ -79,9 +77,12 @@ def get_cve_data(cve):
 
 ### Продукт, вендор
 
+<details>
+
 {% for vendor in d.product_vendor_list %}{{vendor}}
 {% endfor %}
 
+</details>
 
 ### CVSSv3 Score
 
@@ -92,6 +93,7 @@ def get_cve_data(cve):
 {{d.vector}}
 
 ### CPE
+<details>
 
 {% if d.configurations.nodes %}
 {% for conf in d.configurations.nodes %}
@@ -101,16 +103,24 @@ def get_cve_data(cve):
 **AND:**{% endif %}{% for child in children %}{% if child.cpe_match|length > 1 %}**OR:**{% endif %}{% for cpe in child.cpe_match %}
 {{ cpe.cpe23Uri | replace("*", "\*") }}{% endfor %}{% endfor %}{% endfor %}
 {% endif %}
+</details>
+
 ### Links
+<details>
 
 {% for link in d.links %}{{ link }}
 {% endfor %}
 
+
 {% if d.exploit_links %}
+
 ### Exploit
+
 {% for exploit in d.exploit_links %}{{exploit}}
 {% endfor %}
 {% endif %}
+
+</details>
     """
 
     YOU_TRACK_PROJECT_ID = config.get("YOU_TRACK_PROJECT_ID")
@@ -125,22 +135,19 @@ def get_cve_data(cve):
                'Remote Code Execution', 'Memory Corruption', 'Arbitrary code execution', 'CSV Injection',
                'Heap corruption', 'Out of bounds memory access', 'Sandbox escape', 'NULL pointer dereference',
                'Remote Code Execution', 'RCE', 'Authentication Error', 'Use-After-Free', 'Use After Free',
-               'Corrupt Memory', 'Execute Untrusted Code', 'Run Arbitrary Code']
-    
-    r = nvdlib.getCVE(cve, cpe_dict=False)
+               'Corrupt Memory', 'Execute Untrusted Code', 'Run Arbitrary Code', 'heap out-of-bounds write', 'OS Command injection', 'Elevation of Privilege']
 
+    r = nvdlib.getCVE(cve, cpe_dict=False)
     cve_cpe_nodes = r.configurations.nodes
     cpe_nodes = ast.literal_eval(str(r.configurations))
     score = r.v3score
     links = []
     exploit_links = []
     links.append(r.url)
-    
     for t in r.cve.references.reference_data:
         links.append(t.url)
         if 'Exploit' in t.tags:
             exploit_links.append(t.url)
-            
     cpe_for_product_vendors = []
     if cpe_nodes:
         for conf in cve_cpe_nodes:
@@ -151,6 +158,7 @@ def get_cve_data(cve):
             for child in children:
                 for cpe in child.cpe_match:
                     cpe_for_product_vendors.append(cpe.cpe23Uri)
+
 
 #parse CPE--------------------------------------------------------------------------------------------------------------
     product_vendor_list = []
@@ -171,7 +179,6 @@ def get_cve_data(cve):
     for item in version_list:
         if item not in temp1:
             temp1.append(item)
-            
     versions = []
     for item in temp1:
         ver = {"name": item}
@@ -191,6 +198,7 @@ def get_cve_data(cve):
     if not exploit_links:
         value = "Нет"
 
+
 #check regex in cve-----------------------------------------------------------------------------------------------------
     cve_name = ''
     cve_info = r.cve.description.description_data[0].value
@@ -200,6 +208,8 @@ def get_cve_data(cve):
             break
         else:
             cve_name = cve
+
+
 #message----------------------------------------------------------------------------------------------------------------
     data = {
         'cve': cve_info,
@@ -214,8 +224,10 @@ def get_cve_data(cve):
     }
     message = jinja2.Template(template).render(d=data)
 
+
 #check for product_vendor-----------------------------------------------------------------------------------------------
     URL_get_products = config.get("URL_GET_PRODUCTS")
+
     headers = {
         "Accept": "application/json",
         "Authorization": "Bearer {}".format(YOU_TRACK_TOKEN),
@@ -244,6 +256,7 @@ def get_cve_data(cve):
         }
         requests.post(URL_get_products, headers=headers, json=payload)
 
+
 # check for versions----------------------------------------------------------------------------------------------------
     URL_get_vetsions = config.get("URL_GET_VERSIONS")
     headers = {
@@ -270,7 +283,29 @@ def get_cve_data(cve):
         }
         requests.post(URL_get_vetsions, headers=headers, json=payload)
 
+
 # upload information on cve---------------------------------------------------------------------------------------------
+    buff_content = []
+    buff_versions = []
+    if re.search(r'windows', str(product_vendor_list[0])):
+        con = {"name": "Microsoft Windows"}
+        buff_content.append(con)
+        buff_versions = versions
+    elif re.search(r'juniper', str(product_vendor_list[0])):
+        con = {"name": "Juniper"}
+        buff_content.append(con)
+        buff_versions = versions
+    elif re.search(r'adaptive_security_appliance', str(product_vendor_list[0])):
+        con = {"name": "Cisco ASA"}
+        buff_content.append(con)
+        buff_versions = versions
+    else:
+        if content:
+            buff_content.append(content[0])
+        if versions:
+            buff_versions.append(versions[0])
+
+    vector = r.v3vector[9:len(r.v3vector)]
     request_payload = {
         "project": {
             "id": YOU_TRACK_PROJECT_ID
@@ -288,7 +323,7 @@ def get_cve_data(cve):
             {
                 "name": "Продукт (пакет)",
                 "$type": "MultiEnumIssueCustomField",
-                "value": content
+                "value": buff_content
             },
             {
                 "name": "Есть эксплоит",
@@ -298,59 +333,28 @@ def get_cve_data(cve):
             {
                 "name": "Affected versions",
                 "$type": "MultiEnumIssueCustomField",
-                "value": versions
+                "value": buff_versions
             },
             {
                 "name": "CVSS Score",
                 "$type": "SimpleIssueCustomField",
                 "value": score
 
-            }
+            },
+            {
+                "name": "CVSS Vector",
+                "$type": "SimpleIssueCustomField",
+                "value": str(vector)
+
+            },
         ]
     }
-    #print(request_payload) #DEBUG
-    print(requests.post(URL, headers=headers, json=request_payload).json()) # Выгрузка инфы о cve в YouTrack
+    #print(request_payload) #Debug
+    post = requests.post(URL, headers=headers, json=request_payload) # Выгрузка инфы о cve в YouTrack
+    return post.status_code
 
-#-----------------------------------------------MAIN--------------------------------------------------------------------
 
-URL = config.get("URL")
-headers = {
-    "Accept": "application/json",
-    "Authorization": "Bearer {}".format(YOU_TRACK_TOKEN),
-    "Content-Type": "application/json"
-}
-list_summary = requests.get(URL, headers=headers).json() # Получение последних 500 задач с YouTrack
-cve_line = parsing_opencve() # Получение списка новых cve с сайта opencve.io
-# Удаление cve, информация о которых уже есть в YouTrack
-sum_list = []
-for n in range(len(list_summary)):
-    item = list_summary[n]['summary']
-    regex = re.search(r'CVE-\d{4}-\d{4,6}', item)
-    if regex != None:
-        sum_list.append(str(regex.group()))
-
-repeat_list = []
-for item in cve_line:
-    for n in sum_list:
-        if item == n:
-            repeat_list.append(item)
-
-vuln_list = []
-for item in cve_line:
-    if item not in repeat_list:
-        vuln_list.append(item)
-
-chang = []
-for cve in vuln_list:
-    chang.append(get_cve_data(cve))
-
-alert = []
-for item in chang:
-    if item != None:
-        alert.append(item)
-		
-#--------------------------------------------------MAIL_ALERT-----------------------------------------------------------
-if len(alert) > 0:
+def email_alert(cve_list):
     EMAIL_HOST = config.get("EMAIL_HOST")
     EMAIL_PORT = config.get("EMAIL_PORT")
     EMAIL_HOST_PASSWORD = config.get("EMAIL_HOST_PASSWORD")
@@ -359,13 +363,55 @@ if len(alert) > 0:
     msg['Subject'] = 'OpenCVE'
     msg['From'] = EMAIL_HOST_USER
     msg['To'] = config.get("MSG_TO")
-    apchihba = ''
-    for item in alert:
-        apchihba += item + '\n'
-    body = f'Заведено {len(alert)} уязвимостей \n {apchihba}'
+    if len(cve_list) == 1:
+        body = f'Добавлена информация о новой уязвимости {cve_list[0]}'
+    else:
+        body = f'Добавлена информация о новых уязвимостях\n {cve_list}'
     msg.set_content(body)
     msg.set_content(body)
     smtp_server = smtplib.SMTP_SSL(host=EMAIL_HOST, port=EMAIL_PORT)
     smtp_server.login(user=EMAIL_HOST_USER, password=EMAIL_HOST_PASSWORD)
     smtp_server.send_message(msg)
     print('Email sended {}'.format(msg['Subject']))
+
+
+#-----------------------------------------------MAIN--------------------------------------------------------------------
+URL = config.get("URL")
+headers = {
+    "Accept": "application/json",
+    "Authorization": "Bearer {}".format(YOU_TRACK_TOKEN),
+    "Content-Type": "application/json"
+}
+list_summary = requests.get(URL, headers=headers).json() # Получение задач с YouTrack
+broke_cve_line = parsing_opencve() # Получение списка новых cve с сайта opencve.io
+
+broke_sum_list = []
+for n in range(len(list_summary)): #Получение описания для каждой уязвимости
+    broke_sum_list.append(list_summary[n]['summary'])
+
+sum_list = []
+for item in broke_sum_list: #Выдергивание идентификатора CVE из каждого описания уязвимости
+    regex = re.search(r'CVE-\d{4}-\d{4,8}', item)
+    if regex:
+        sum_list.append(str(regex.group()))
+
+cve_line = []
+for cve in broke_cve_line: # Удаление лишеного пробела в конце идентификаторов CVE
+    cve_line.append(cve[:-1])
+
+vuln_list = []
+for item in cve_line: #Удаление идентификаторов CVE по которым уже заведены задачи
+    if item not in sum_list:
+        vuln_list.append(item)
+
+
+cve_list = []
+for i in range(len(vuln_list)): # Заведение задач в YouTrack
+    cve = vuln_list[i]
+    post = get_cve_data(cve)
+    if post == 200:
+        cve_list.append(cve)
+    print(f'{i + 1} / {len(vuln_list)} - {post}')
+
+if cve_list:
+    email_alert(cve_list)
